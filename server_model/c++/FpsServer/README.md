105c638da20aa5b6870fdd158e00f34da64166b1
qwe624758472 <624758472@qq.com>
2021-03-19 17:24:11 +0800

    1.加入elf解析。2.加入游戏服务器代码

---
+++ server_model/c++/FpsServer/README.md
@@ -0,0 +1,2 @@
+# FpsServer
+init
---
+++ server_model/c++/FpsServer/server/ServerConnect.h
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <string.h>
+#include <sys/select.h>
+#include <pthread.h>
+using namespace std;
+
+
+//该结构体保存的位置。
+typedef struct _playerLocation
+{
+    float x; //x轴世界坐标。
+    float z; //y轴世界坐标。
+}playerLocation;
+
+
+ClientInfo[];
+
+class ServerConnect
+{
+public:
+    ServerConnect();
+    ~ServerConnect();
+
+
+    int port = 0;  //端口号。
+    int N = 0;     //连接数。
+
+    int server_sock = 0;
+
+
+    int ServerSocketInit();    
+    int max_fd(int a[], int n);
+};
+
